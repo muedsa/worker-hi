34456be174b24fdf78333d76f48833ba5311b003
MUedsa <7676275+muedsa@users.noreply.github.com>
2022-08-09 11:45:23 +0800

    Initial commit

---
+++ src/ResponseUtil.js
@@ -0,0 +1,46 @@
+const BASE_404 = () => new Response('Not Found.', { status: 404 });
+
+const json = (data) => new Response(JSON.stringify(data), {
+    headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+});
+const jsonSuccess = (data) => json(JsonResponse.success(data));
+
+const jsonError = (data) => json(JsonResponse.error(data));
+
+const JsonResponse = {
+    success(data){
+        return {
+            code: 0,
+            msg: 'success',
+            data: data,
+        }
+    },
+    error(code, msg){
+        return {
+            code: code,
+            msg: msg,
+        }
+    }
+}
+
+const text = (data) => new Response(data, {
+    headers: {
+        'Content-Type': 'text/plain; charset=utf-8'
+    }
+});
+
+const html = (data) => new Response(data, {
+    headers: {
+        'Content-Type': 'text/html; charset=utf-8'
+    }
+});
+
+export default {
+    BASE_404,
+    jsonSuccess,
+    jsonError,
+    text,
+    html
+};
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import { Router } from 'itty-router'
+import Res from "./ResponseUtil";
+// Create a new router
+const router = Router();
+
+// errorHandler
+const errorHandler = error => Res.jsonError(error.status || 500, error.message || 'Server Error');
+
+// github repository api
+const GITHUB_REPOSITORY_RELEASE_LATEST_URL = "https://github.com/${user}/${repo}/releases/latest"
+const HEADER = {
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+	"Accept": "*/*"
+}
+router.get("/github/:user/:repo/release/latest", async ({ params }) => {
+	const url = GITHUB_REPOSITORY_RELEASE_LATEST_URL
+		.replace("${user}", params.user)
+		.replace("${repo}", params.repo);
+	const response = await fetch(url, {
+		method: 'GET',
+		headers: HEADER,
+	});
+	const html = await response.text();
+	const iterator = html.matchAll(/<meta property="og:(.*?)"\s*content="((?:.|\n)*?)"\s*\/>/g);
+	let data = {};
+	for (let item of iterator) {
+		data[item[1]] = item[2];
+		if(item[1] === 'url') {
+			let paths = item[2].split('/');
+			if(paths[paths.length - 2] === 'tag'){
+				data.tag = paths[paths.length - 1];
+			}
+		}else if(item[1] === 'image:width' || item[1] === 'mage:height'){
+			data[item[1]] = Number.parseInt(item[2]);
+		}
+	}
+	return Res.jsonSuccess(data);
+});
+
+// 404 for everything else
+router.all('*', () => Res.BASE_404());
+
+/*
+This snippet ties our worker to the router we deifned above, all incoming requests
+are passed to the router where your routes are called and the response is sent.
+*/
+addEventListener('fetch', (e) => {
+	e.respondWith(router.handle(e.request).catch(errorHandler))
+});
